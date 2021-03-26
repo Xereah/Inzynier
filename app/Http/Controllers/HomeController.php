03c658e81d49c5711e06a39aed1e3e3c3fd9fb72
Xereah <patrykstruzik@onet.pl>
2021-03-26 18:41:21 +0100

    commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,7 +22,7 @@ class HomeController extends Controller
 
     /**
      * Show the application dashboard.
-     *
+     *hgjhgjhg
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
