448a566db447afd7ea1285a285b554158e057c29
Xereah <patrykstruzik@onet.pl>
2021-03-29 21:43:49 +0200

    modal

--- resources/views/FrontEnd/Home/ProduktWidok.blade.php
+++ resources/views/FrontEnd/Home/ProduktWidok.blade.php
@@ -48,6 +48,43 @@
       <div class="table-responsive mb-2">
       @if($produkty->Ilosc ==0)
       <h5>Niestety nie mamy już tego produktu na stanie<h5>
+      <button type="button" class="btn btn-primary col-sm-12" data-toggle="modal" data-target="#exampleModal">
+                                Kiedy dostępny?
+                            </button>
+
+                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                         <div class="modal-content">
+                          <div class="modal-header">
+                             <h5 class="modal-title" id="exampleModalLabel">Powiadom mnie o dostępności</h5>
+                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                  </button>
+                           </div>
+                          <div class="modal-body">
+                          <div class="row p-2 bg-white ">
+                          <div class="col-md-5 mt-1"><img class="img-fluid img-responsive rounded product-image" src="{{ asset($produkty->Zdjecie) }}"></div>
+                          <div class="col-md-6 mt-1">
+                              <h5>{{ $produkty->Nazwa }}</h5>
+                              {{ $produkty->Opis }}
+                              <br>
+                              <br>
+                          </div> 
+                          
+                        
+                            <div class="input-group">
+                            
+                                <input type="text" class="form-control" placeholder="Wpisz adres email">
+                                <div class="input-group-append">
+                                     <button class="btn btn-secondary" type="button">
+                                     <i class="fas fa-envelope"></i>
+                                     </button>
+                                    
+                                 </div>
+                                 <small id="emailHelp" class="form-text text-muted">Na podany email zostanie wysłana wiadmość gdy produkt będzie dostępny.</small>
+                             </div>
+                          </div> 
+                     </div>
       @else
       <h5>Dostępna ilość: {{$produkty->Ilosc}}{{$produkty->JednostkaMiary}}</h5>
       @endif
