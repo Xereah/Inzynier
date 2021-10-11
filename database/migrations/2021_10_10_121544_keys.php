876fc906eb63028a4f7140d85cc3879cf37c95f4
Xereah <patrykstruzik@onet.pl>
2021-10-11 19:51:35 +0200

    klucze obce

---
+++ database/migrations/2021_10_10_121544_keys.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Keys extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('produkty', function (Blueprint $table){
+            $table->foreign('fk_kategorie')->references('id')->on('kategorie');
+
+        });
+
+     
+        Schema::table('zamowienie', function (Blueprint $table){
+            $table->foreign('fk_uzytkownik')->references('id')->on('users');
+
+        });
+
+        Schema::table('zamowienie', function (Blueprint $table){
+            $table->foreign('fk_platnosc')->references('id')->on('platnosc');
+
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
